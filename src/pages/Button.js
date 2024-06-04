d58aaea5782b2ca0c680149315fa901746305fae
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2024-06-04 07:23:42 +0530

    Add files via upload

---
+++ src/pages/Button.js
@@ -0,0 +1,36 @@
+// Button.js
+import React, { useState } from "react";
+import styles from "./Button.module.css";
+
+const Button = ({ url }) => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  return (
+    <div>
+      <button className={styles.button} onClick={togglePopup}>
+        Open Popup
+      </button>
+      {showPopup && (
+        <div className={styles.popup}>
+          <button className={styles.closeButton} onClick={togglePopup}>
+            Close
+          </button>
+          <iframe
+            src={url}
+            width="100%"
+            height="400px"
+            frameBorder="0"
+            allowFullScreen
+            title="Embedded Content"
+          ></iframe>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Button;
