f0037e847503b53e62c075d66b38992e4d8cf74b
mk veerendra <136776337+MKVEERENDRA@users.noreply.github.com>
2024-06-04 08:11:04 +0000

    new 1:40

---
+++ public/component---src-pages-404-js-6ee388843531940a90ad.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[125],{3331:function(t,e,n){n.r(e),n.d(e,{Head:function(){return s}});var a=n(6540),u=n(4728),l=n(7528);const s=()=>a.createElement(l.A,{title:"404: Not Found"});e.default=()=>a.createElement(u.A,null,a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-6ee388843531940a90ad.js.map
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Layout from "../components/layout";
-import { Helmet } from 'react-helmet';
+import { Helmet } from "react-helmet";
 import "./home.css";
 import PickaxeButton from "../components/Pix";
 
@@ -18,17 +18,16 @@ export default function Contact() {
         inquiries, feel free to reach out to us through the following channels:
       </p>
       <hr />
-       {/* Button */}
-<div className="button-container">
-    <button className="popup-button" onClick={togglePopup}>
-    <span className="emoji">💬</span> 
-      Chat With Our Company AI
-    </button>
-  </div>
-
-{/* Popup */}
-{showPopup && (
-  <div className="popup-overlay">
+      {/* Button */}
+      <div className="button-container">
+        <button className="popup-button" onClick={togglePopup}>
+          <span className="emoji">💬</span>
+          Chat With Our Company AI
+        </button>
+      </div>
+      {/* Popup */}
+      {showPopup && (
+        <div className="popup-overlay">
           <div className="popup-content">
             <button className="close-button" onClick={togglePopup}>
               Close X
@@ -37,28 +36,43 @@ export default function Contact() {
               Close X
             </button>
             <iframe
-src="https://www.blackbox.ai/agent/RAJARATHANAMIjUXsf3"              width="100%"
+              src="https://www.blackbox.ai/agent/RAJARATHANAMIjUXsf3"
+              width="100%"
               height="600px"
               frameBorder="0"
               allowFullScreen
               title="Pickaxe Embed"
             ></iframe>
           </div>
-        </div>)}
+        </div>
+      )}
       <h2>Email us!</h2>
       <p>For general inquiries or consulting services:</p>
       <i className="fa-regular fa-envelope"></i>&nbsp;
-      <a className="regular-link" href="mailto:mkrveeresh@gmail.com" target="_blank" rel="noreferrer">
-Click to Connect with Owner      </a>
+      <a
+        className="regular-link"
+        href="mailto:mkrveeresh@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click to Connect with Owner{" "}
+      </a>
       <p>For support around our products and prior provided services:</p>
       <i className="fa-regular fa-envelope"></i>&nbsp;
-      <a className="regular-link" href="mailto:veerendravamsi66@gmail.com" target="_blank" rel="noreferrer">
-      Click to Connect with Owner        </a>
+      <a
+        className="regular-link"
+        href="mailto:veerendravamsi66@gmail.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click to Connect with Owner{" "}
+      </a>
       <br />
       <h2>Connect with us!</h2>
       <p>Follow or contact us on our social platforms:</p>
       <i className="fab fa-github"></i>&nbsp;
-      <a className="regular-link"
+      <a
+        className="regular-link"
         href="https://github.com/MKVEERENDRA"
         target="_blank"
         rel="noreferrer"
@@ -67,7 +81,8 @@ Click to Connect with Owner      </a>
       </a>
       <br /> <br />
       <i className="fab fa-linkedin"></i>&nbsp;
-      <a className="regular-link"
+      <a
+        className="regular-link"
         href="https://www.linkedin.com/in/veerendra-vamshi-821890225/"
         target="_blank"
         rel="noreferrer"
@@ -75,44 +90,53 @@ Click to Connect with Owner      </a>
         OWNER on LinkedIn
       </a>
       <br /> <br />
-      <br/>
+      <br />
       <h2>Share our website!</h2>
-      <p>Know somebody that may need our help? Share the RajaRathanam website with others:</p>
+      <p>
+        Know somebody that may need our help? Share the RajaRathanam website
+        with others:
+      </p>
       <i class="fa-solid fa-briefcase"></i>&nbsp;
-      <a className="regular-link"
-         href="https://urukundha.com" 
-         target="_blank" 
-         rel="noreferrer">
-         RajaRathanam.in
+      <a
+        className="regular-link"
+        href="https://urukundha.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        RajaRathanam.in
       </a>
       <br />
       <br />
       <hr />
       <div className="closing-wrapper">
-        <p>Like this website? Feel free to...<br />
-        <br />
-        <i id="github-fork" className="fa-solid fa-code-fork fa-bounce"></i>&nbsp;&nbsp;
-          <a className="regular-link"
+        <p>
+          Like this website? Feel free to...
+          <br />
+          <br />
+          <i id="github-fork" className="fa-solid fa-code-fork fa-bounce"></i>
+          &nbsp;&nbsp;
+          <a
+            className="regular-link"
             href="https://github.com/MKVEERENDRA"
             target="_blank"
             rel="noreferrer"
           >
             Fork and Star it on GitHub!
           </a>
-          &nbsp;&nbsp;<i id="github-star" className="fa-solid fa-star fa-bounce"></i>
-        </p>  
+          &nbsp;&nbsp;
+          <i id="github-star" className="fa-solid fa-star fa-bounce"></i>
+        </p>
         <a href="https://github.com/MKVEERENDRA">
           <img
             src="/images/company/R.gif"
-            alt="Night Owl Labs"
-            title="Night Owl Labs"
+            alt="RAja RajaRathanam"
+            title="RajaRathanam"
             className="landing-logo"
             id="landing-footer"
-            style={{ height: '200px', width: '200px' }}
-
+            style={{ height: "200px", width: "200px" }}
           />
-          </a>
+        </a>
       </div>
     </Layout>
-  )
+  );
 }
